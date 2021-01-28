24ae0b9bbb1970c90af4c44bf286ae3c0025662d
Kajal2003 <singhkaju2099@gmail.com>
2021-01-28 15:10:41 +0530

    First Commit
     Uploading All Files

---
+++ src/main/java/com/cap2/DemoMaven/App.java
@@ -0,0 +1,13 @@
+package com.cap2.DemoMaven;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
